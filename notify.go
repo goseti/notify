453a2072eacdee54f100781294f92827a153da16
Li Yan <liyan34@lenovo.com>
2017-07-12 15:05:37 +0800

    remove observer

--- notify.go
+++ notify.go
@@ -8,15 +8,30 @@ type ObserverFunc func(...interface{})
 
 var hooks map[string]map[uint64]ObserverFunc = make(map[string]map[uint64]ObserverFunc)
 
-func AddObserver(notify string, observer ObserverFunc) {
+func observerKey(observer ObserverFunc) uint64 {
     pt := unsafe.Pointer(&observer)
-    key := uint64(*(*uintptr)(pt))
+    return uint64(*(*uintptr)(pt))
+}
+
+func AddObserver(notify string, observer ObserverFunc) {
+    key := observerKey(observer)
 
     if _, ok := hooks[notify]; !ok {
         hooks[notify] = make(map[uint64]ObserverFunc)
     }
 
     hooks[notify][key] = observer
+}
+
+func RemoveObserver(notify string, observer ObserverFunc) bool {
+    key := observerKey(observer)
+    if hk, ok := hooks[notify]; ok {
+        if _, ok := hk[key]; ok {
+            delete(hk, key)
+            return true
+        }
+    }
+    return false
 }
 
 func Publish(notify string, args ...interface{}) {
